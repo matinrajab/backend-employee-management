77bc0e466cc6a8d19c2946bb4401931db2075c58
Matin Muhammad <matinrajab.mr@gmail.com>
2025-10-16 12:48:48 +0700

    wrap endpoint with middleware sanctum

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::get('employees', [EmployeeController::class, 'index']);
-Route::get('search-employees', [EmployeeController::class, 'search']);
-Route::post('employee', [EmployeeController::class, 'store']);
-Route::get('employee/{id}', [EmployeeController::class, 'show']);
-Route::get('work-unit/{id}', [EmployeeController::class, 'showByWorkUnit']);
-Route::put('employee/{id}', [EmployeeController::class, 'update']);
-Route::delete('employee/{id}', [EmployeeController::class, 'destroy']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('employees', [EmployeeController::class, 'index']);
+    Route::get('search-employees', [EmployeeController::class, 'search']);
+    Route::post('employee', [EmployeeController::class, 'store']);
+    Route::get('employee/{id}', [EmployeeController::class, 'show']);
+    Route::get('work-unit/{id}', [EmployeeController::class, 'showByWorkUnit']);
+    Route::put('employee/{id}', [EmployeeController::class, 'update']);
+    Route::delete('employee/{id}', [EmployeeController::class, 'destroy']);
+});
